05662c2ad0c434ba9669b86ab074eb2fba819bab
Jayce-Ping <315229706@qq.com>
2025-08-18 23:45:04 +0800

    Fix async bug

--- flow_grpo/flow_grpo/rewards/rewards.py
+++ flow_grpo/flow_grpo/rewards/rewards.py
@@ -109,6 +109,7 @@ def imagereward_score(device):
     return _fn
 
 def consistency_score(device):
+    import asyncio
     from flow_grpo.rewards.consistency_scorer import ConsistencyScorer
 
     scorer = ConsistencyScorer(
@@ -124,7 +125,15 @@ def consistency_score(device):
             images = images.transpose(0, 2, 3, 1)  # NCHW -> NHWC
             images = [Image.fromarray(image) for image in images]
 
-        scores = scorer(images, prompts, metadatas)
+        loop = asyncio.get_event_loop()
+        if loop.is_running():
+            future = asyncio.run_coroutine_threadsafe(
+                scorer(images, prompts, metadatas), loop
+            )
+            scores = future.result()
+        else:
+            scores = loop.run_until_complete(scorer(images, prompts, metadatas))
+
         return scores, {}
 
     return _fn
