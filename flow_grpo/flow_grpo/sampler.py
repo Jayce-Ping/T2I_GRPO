0c17d5a44845cf853b6e850c6c97c8a105adea58
Jayce-Ping <315229706@qq.com>
2025-08-20 07:38:10 +0800

    update

--- flow_grpo/flow_grpo/sampler.py
+++ flow_grpo/flow_grpo/sampler.py
@@ -2,7 +2,8 @@ import math
 import torch
 from torch.utils.data import Sampler, Dataset
 
-class DistributedKRepeatSampler(Sampler):
+class DistributedKRepeatSamplerOld(Sampler):
+    # This class originally used in FlowGRPO.
     def __init__(self, dataset : Dataset, batch_size : int, k : int, num_replicas : int, rank : int, seed :int = 0):
         self.dataset = dataset
         self.batch_size = batch_size  # Batch size per replica
@@ -49,7 +50,12 @@ class DistributedKRepeatSampler(Sampler):
 
 
 
-class DistributedKRepeatSamplerNew(Sampler):
+class DistributedKRepeatSampler(Sampler):
+    """
+    This class is a new implementation of the distributed K-repeat sampler, used in current code.
+    Where the number of unique samples is determined by the total number of replicas and the batch size,
+    and k can be not divisible by n*b.
+    """
     def __init__(self, dataset : Dataset, batch_size : int, k : int, num_replicas : int, rank : int, seed :int = 0):
         self.dataset = dataset
         self.batch_size = batch_size  # Batch size per replica
@@ -62,7 +68,6 @@ class DistributedKRepeatSamplerNew(Sampler):
         self.total_samples = self.num_replicas * self.batch_size
         if self.total_samples % self.k == 0:
            # k | n*b, which means all repetitions of one sample can be yielded in one iteration
-           assert self.total_samples % self.k == 0, f"k can not divide n*b, k{k}-num_replicas{num_replicas}-batch_size{batch_size}"
            self.m = self.total_samples // self.k  # Number of unique samples
            self.min_iter = 1
         else:
@@ -79,30 +84,26 @@ class DistributedKRepeatSamplerNew(Sampler):
         while True:
             # Generate a deterministic random sequence to ensure all replicas are synchronized
             g = torch.Generator()
-            g.manual_seed(self.seed + self.epoch)
+            g.manual_seed(self.seed + self.epoch // self.min_iter) # Divide min_iter
             
             # Randomly select m unique samples
             indices = torch.randperm(len(self.dataset), generator=g)[:self.m].tolist()
-            
+
             # Repeat each sample k times to generate m*k total samples.
             repeated_indices = [idx for idx in indices for _ in range(self.k)]
             
             # Shuffle to ensure uniform distribution
             shuffled_indices = torch.randperm(len(repeated_indices), generator=g).tolist()
             shuffled_samples = [repeated_indices[i] for i in shuffled_indices]
-            
-            # Split samples to each replica
-            for it in range(self.min_iter):
-                offset = it * self.batch_size * self.num_replicas
-                per_card_samples = []
-                for i in range(self.num_replicas):
-                    start = i * self.batch_size + offset
-                    end = start + self.batch_size + offset
-                    per_card_samples.append(shuffled_samples[start:end])
-                
-                # Return current replica's sample indices
-                yield per_card_samples[self.rank]
-    
+
+            # Offset for current epoch
+            offset = (self.epoch % self.min_iter) * self.batch_size * self.num_replicas
+
+            # Compute start and end indices for current replica
+            start = offset + self.rank * self.batch_size
+            end = start + self.batch_size
+            yield shuffled_samples[start:end]
+
     def set_epoch(self, epoch : int):
         self.epoch = epoch  # Used to synchronize random state across epochs
 
@@ -112,13 +113,13 @@ class DistributedKRepeatSamplerNew(Sampler):
 if __name__ == "__main__":
     from flow_grpo.datasets.prompt_dataset import TextPromptDataset
     train_batch_size = 1
-    num_image_per_prompt = 4
-    num_processes = 4
+    num_image_per_prompt = 2
+    num_processes = 2
 
     dataset = 'dataset/ocr'
     train_dataset = TextPromptDataset(dataset, 'train')
     train_samplers = [
-        DistributedKRepeatSamplerNew( 
+        DistributedKRepeatSampler(
         dataset=train_dataset,
         batch_size=train_batch_size,
         k=num_image_per_prompt,
@@ -127,12 +128,19 @@ if __name__ == "__main__":
         seed=42)
         for i in range(num_processes)
     ]
+    print(train_samplers[0].m, train_samplers[0].total_samples)
+    counter = {}
 
-    for epoch in range(3):
-        for sampler in train_samplers:
+    for epoch in range(12):
+        for i, sampler in enumerate(train_samplers):
             sampler.set_epoch(epoch)
-        
-        for i, sample_indices in enumerate(train_samplers[0]):
-            print(f"Epoch {epoch}, Process {i}: Sample Indices: {sample_indices}")
-            if i == 5:  # Limit to 5 iterations for demonstration
-                break
+            sampler_iter = iter(sampler)
+            sampled_indices = next(sampler_iter)
+            for index in sampled_indices:
+                if index not in counter:
+                    counter[index] = 0
+                counter[index] += 1
+
+            print(f"Epoch {epoch}, Sampler {i}: Sampled indices: {sampled_indices}")
+
+    print(counter)
